2597577df6a28b269604374956bcae9709f8d254
rlavinsh <rlavinsh@gmail.com>
2024-09-12 13:31:46 +0530

    completed variables and datatypes and commenting and printing statment

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        String _name = "Hello";
+//        String $last = "coders";
+//        String @first = "coders";
+        System.out.println(_name);
+
+        // single line comment
+        //        /**/ Multiline comment
+        byte a = 5;
+        short b = 6;
+        int c = 10;
+        long d = 15416;
+        float e = 563.242f;
+        double f = 4554.45;
+        boolean g = true;
+        char h = 'R';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
