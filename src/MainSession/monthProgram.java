1e16f5dff64600a8864656593647c247fcadb73f
rlavinsh <rlavinsh@gmail.com>
2024-09-19 15:10:31 +0530

    question updated

---
+++ src/MainSession/monthProgram.java
@@ -0,0 +1,11 @@
+package MainSession;
+
+public class monthProgram {
+    /*
+    You are tasked with writing a program that identifies the name of a month based on the input provided by the user. The user will enter a number between 1 and 12, where each number corresponds to a month of the year.
+Input Range:
+If the user inputs a number between 1 and 8, your program should print the corresponding month name (January to August).
+For numbers greater than 8, the program should execute the default case, outputting a message like "Invalid input or Month beyond August."
+
+    * */
+}
