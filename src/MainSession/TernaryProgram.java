d523e46e2b76eb39f6bf954cb938b7fba72b5d46
rlavinsh <rlavinsh@gmail.com>
2024-09-19 15:36:10 +0530

    ternary operator

--- src/MainSession/TernaryProgram.java
+++ src/MainSession/TernaryProgram.java
@@ -1,2 +1,20 @@
-package MainSession;public class TernaryProgram {
+package MainSession;
+
+import java.util.Scanner;
+
+public class TernaryProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int num = sc.nextInt();
+//        if (num%2==0){
+//            System.out.println("Even");
+//        }else {
+//            System.out.println("Odd");
+//        }
+
+        // Convert the same program by using ternary operator
+        String result = (num%2==0)?"Even":"Odd";
+        System.out.println(result);
+    }
 }
